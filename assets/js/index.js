2440ac5ba4d0ce9a7177a6ca5315aafd6e7711a0
scelsic2 <scelsic2@gmail.com>
2023-08-25 16:29:18 -0400

    working on recognizing first card for flip bback

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,5 @@
 const cardSection = document.querySelector('.card-section')
+const card = document.querySelectorAll('.card')
 const cardBack = document.querySelectorAll('.card-back')
 
 const numberArray1 = [1, 2, 3, 4, 5, 6, 7, 8]
@@ -16,8 +17,7 @@ function flipCard(e) {
         selectedCard.classList.add('flip')
         firstPick = e.target.parentElement.children[1].innerText
         selectedCard.dataset.attribute = 0
-        console.log('-----first pick-----')
-        console.log(firstPick)
+
     } else {
         const selectedCard = e.target.parentElement.parentElement
         selectedCard.classList.add('flip')
@@ -26,13 +26,31 @@ function flipCard(e) {
         if (secondPick == firstPick) {
             console.log(`It's a match!`)
         } else {
-            firstPick = null
+            setInterval(function() {
+                selectedCard.classList.remove('flip')
+              }, 2000);
+              
+            for (i = 0; i < card.length; i++) {
+                let dataAttribute = card[i].attributes[1]
+                
+                if (dataAttribute == 0) {
+                    console.log('-----dataAttribute-----')
+                    console.log(dataAttribute)
+                    
+                } else {
+                    setInterval(function() {
+                        selectedCard.classList.remove('flip')
+                        console.log(cardAtIndex)
+                        // card[i].classList.remove('flip')
+                      }, 2000);
+                      card[i].removeAttribute('data-attribute')
+                }
+            }
+              firstPick = null
+              secondPick = null
         }
-        console.log('-----second pick-----')
-        console.log(secondPick)
     }
-        
-    return firstPick
+    return firstPick, secondPick
 }
 
 function assignCards() {
